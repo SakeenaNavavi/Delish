6a24d425bdf140428bafe151209ba1e70b092590
NishadiSandeepani <nishadisandeepani26@gmail.com>
2023-09-13 11:23:38 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Home from './pages/home';
 import Layout from './pages/Layout';
 import Reservation from './pages/Reservation';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Footer from './components/molecules/Footer';
 
 function App(){
   return (
 
       <div className="App">
       <NavBar/>
+      <Footer/>
       <BrowserRouter>
       <Routes>
       <Route path="/" element={<Home />} />
