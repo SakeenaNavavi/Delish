27769915370f1c59a04b58836c6dbb44ce12efa4
NishadiSandeepani <nishadisandeepani26@gmail.com>
2023-09-19 14:27:54 +0530

    added and styled the Breakfast Menu Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Reservation from './pages/Reservation';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Footer from './components/molecules/Footer';
 import ContactUs from './pages/ContactUs';
+import Breakfast from './pages/Breakfast';
 
 function App(){
   return (
@@ -21,6 +22,7 @@ function App(){
         <Route path="/" element={<Layout/>}/>
         <Route path="Reservation" element={<Reservation />} />
         <Route path="ContactUs" element={<ContactUs />} />
+        <Route path="Breakfast" element={<Breakfast />} />
       </Routes>
       </BrowserRouter>
     </div>
